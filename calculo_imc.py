e6849802df5d70d7eea8496bb412d4b8ae3160f9
morangoacido11 <ratoeirodasilvalindo9590@gmail.com>
2026-06-03 19:06:37 -0300

    v0.1

--- calculo_imc.py
+++ calculo_imc.py
@@ -1,1 +1,14 @@
+#Calcular IMC
+print("="*30)
+print("Calculador de IMC")
+print("="*30)
 
+def calcular_imc(peso,altura):
+    imc = peso/(altura*altura)
+    return imc
+def classificar_imc(valor_imc):
+    if valor_imc < 25:
+        return "NORMAL"
+    else:
+        return "ACIMA DO PESO"
+    
