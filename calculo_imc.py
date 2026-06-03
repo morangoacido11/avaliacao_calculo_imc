dd91eed29de414c696bfa8092b40cd572da6a7a3
morangoacido11 <ratoeirodasilvalindo9590@gmail.com>
2026-06-03 20:12:22 -0300

    v0.2

--- calculo_imc.py
+++ calculo_imc.py
@@ -1,4 +1,5 @@
 #Calcular IMC
+#Ana Gabriely - Calculos
 print("="*30)
 print("Calculador de IMC")
 print("="*30)
@@ -19,3 +20,6 @@ def gerar_aviso(status, imc_valor):
         return "muito bem seu IMC é normal! mantenha-se saudável"
     else:
      return "seu IMC é acima de 25.., cultive uma rotina equilibrada! isso manterá sua saúde em dia!!!"
+#Ana Gabriely - Variaveis (peso,altura)
+peso = float(input("Insira seu peso: "))
+altura = float(input("Insira sua altura: "))
